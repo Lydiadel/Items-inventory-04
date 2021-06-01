5ae439228b1cee6d4eba94545ca4302e28311859
Lydia <lydia101-lidi@hotmail.com>
2021-05-31 21:52:02 -0600

    [FEAT] Main

---
+++ Tarea4_Inventario.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  SituacionProblema
+//
+//  Created by Lydia Delgado Uriarte & Hugo Edgar Palomares Estrella on 14/05/21.
+//
+
+#include <iostream>
+#include "Personaje.h"
+#include "Hechicera.h" 
+#include "Vikingo.h"
+#include "Arquero.h"
+using namespace std;
+#include <vector>
+
+int main(){
+    vector <Personaje*> vecPersonajes;
+    Personaje *clasesPersonajes;
+
+    clasesPersonajes = new Hechicera(0,0,"",100);
+    vecPersonajes.push_back(clasesPersonajes);
+    clasesPersonajes = new Arquero(0,0,"",100);
+    vecPersonajes.push_back(clasesPersonajes);
+    clasesPersonajes = new Vikingo(0,0,"",100);
+    vecPersonajes.push_back(clasesPersonajes);
+
+    for (int i=0; i<vecPersonajes.size();i++){
+        vecPersonajes[i]->shoot();
+    }
+
+    return 0;
+}
